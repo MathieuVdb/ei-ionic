6707630adcd9cb4f8988b8b504ee732d561778e5
Mathieu Vdb <Soras@live.fr>
2022-05-10 14:38:59 +0200

    Search bar

--- src/app/pages/personnages/personnages.page.ts
+++ src/app/pages/personnages/personnages.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { Personnage } from 'src/app/utils/models/personnage';
 import { PersonnagesService } from 'src/app/utils/services/personnages.service';
 
@@ -10,20 +11,30 @@ import { PersonnagesService } from 'src/app/utils/services/personnages.service';
 })
 export class PersonnagesPage implements OnInit {
 
-  personnages$: Observable<Personnage[]>;
-
+  //personnages$: Observable<Personnage[]>;
+  personnages: Personnage[];
+  displayed: Personnage[];
+  search= '';
 
   constructor(
     private personnageService: PersonnagesService
   ) { }
 
   ngOnInit() {
-    /* Méthode 1
+    /* Méthode 1*/
     this.personnageService.getAll().
-      subscribe(perso => this.personnages = perso);*/
+      subscribe(perso => {
+        this.personnages = perso;
+        this.displayed = perso;
+      });
 
-      /* Méthode 2 */
-      this.personnages$ = this.personnageService.getAll();
+      /* Méthode 2
+      this.personnages$ = this.personnageService.getAll(); */
+  }
+
+  filterArray() {
+    this.displayed = (!this.search) ? 
+        this.personnages : this.personnages.filter(p => p.name.toLowerCase().includes(this.search.toLowerCase().trim()))
   }
 
 }
